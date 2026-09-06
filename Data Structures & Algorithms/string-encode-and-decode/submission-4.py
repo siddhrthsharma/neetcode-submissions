9bf4d55c6b50955bf4cbabb38302dda87962f816
Siddharth Sharma <siddharthsharmaa25@gmail.com>
2026-09-05 22:36:27 -0700

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.py
@@ -0,0 +1,22 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        encoded_string = ""
+
+        for string in strs:
+            encoded_string += str(len(string)) + "$" + string
+
+        return encoded_string
+
+    def decode(self, s: str) -> List[str]:
+        output = []
+        i = 0
+        while i < len(s):
+            j = i
+            while s[j] != "$":
+                j += 1
+            length = int(s[i:j])
+            output.append(s[j + 1: j + 1 + length])
+            i = j + 1 + length
+
+        return output
